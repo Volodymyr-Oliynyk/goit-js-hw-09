5e4e32b56e06123d7a0407416ac7e6064b7361e2
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-23 18:54:58 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    // console.log('color change');
+  }, 1000);
+  startBtn.disabled = true;
+
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
