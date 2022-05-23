fe2d1c43c12de7d53e64f15892ffd8a3cd23db5b
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-24 00:45:35 +0300

    purt 1 task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  startBtn: document.querySelector('[data-start]'),
+  input: document.querySelector('input#datetime-picker'),
+};
+console.log(refs.input);
+let intervalId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const date = new Date();
+    if (selectedDates[0].getTime() < date.getTime()) {
+      refs.startBtn.disabled = true;
+      return Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+
+const pickr = flatpickr(refs.input, options);
+
+function onBtnStartClick() {
+  intervalId = setInterval(() => {
+    const today = new Date();
+    const deadline = pickr.selectedDates[0];
+    const delta = deadline.getTime() - today.getTime();
+    if (delta < 0) {
+      clearInterval(intervalId);
+      return;
+    }
+    const convertedTimerValue = convertMs(delta);
+    updateTimerValue(convertedTimerValue);
+    startBtn.disabled = true;
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+refs.start.addEventListener('click', onBtnStartClick);
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
